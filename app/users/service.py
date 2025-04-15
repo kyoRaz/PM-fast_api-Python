bbf2d3e41a4727f32ce535df36ae695158deeda6
Kyo Steeve <80814641+kyoRaz@users.noreply.github.com>
2025-04-15 17:09:59 +0300

    refactor : injection de dependance

--- app/users/service.py
+++ app/users/service.py
@@ -1,18 +1,31 @@
-from sqlmodel import select
+from sqlmodel import select , Session
 from app.core.database import get_session
 from app.users.models import User
-from fastapi import Depends
 from uuid import UUID
 
 
-def create_user(user: User, session = Depends(get_session)):
+def create_user(user: User, session = Session):
     session.add(user)
     session.commit()
     session.refresh(user)
     return user
 
-def get_all_users(session = Depends(get_session)):
+def get_all_users(session = Session):
     return session.exec(select(User)).all()
 
-def get_user_by_id(user_id: UUID, session = Depends(get_session) ) -> User | None:
+def get_user_by_id(user_id: UUID, session = Session ) -> User | None:
     return session.get(User, user_id)
+
+
+def updateser(user_id: UUID, updated_data: dict, session = Session ) -> User | None:
+    user = session.get(User, user_id)
+    if not user:
+        return None
+
+    for key, value in updated_data.items():
+        setattr(user, key, value)
+
+    session.add(user)  
+    session.commit()
+    session.refresh(user)
+    return user
